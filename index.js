c5972ca6d7d700eda374334dec01587bdbd68daa
mrhl082024 <mrhl082024@gmail.com>
2025-02-04 13:15:53 +0100

    first

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
-
-
 /* Funksjon for å generere et array av pr */
-const randomProducts = () =>
-{
-    const productArr = [];
-    const randomNum = Math.ceil(Math.random() * 100);
-    for (let i = 0; i < randomNum; i++)
-    {
-        productArr.push(
-            {
-                cost: Math.ceil(Math.random()*100),
-                ammount: Math.ceil(Math.random()*10)
-            }
-        )
-    }
-    return productArr;
-}
+const randomProducts = () => {
+  const productArr = [];
+  const randomNum = Math.ceil(Math.random() * 100);
+  for (let i = 0; i < randomNum; i++) {
+    productArr.push({
+      cost: Math.ceil(Math.random() * 100),
+      ammount: Math.ceil(Math.random() * 10),
+    });
+  }
+  return productArr;
+};
 
 const products = randomProducts();
 console.log(products);
 
 /* Her mangler vi en funksjon som kan ta inn produktene, og et potensielt avslag, og så returnere totalsummen av alle produktene (cost * ammount). */
 
-/* Som sist har du kun lov å legge til din egen funksjon, den skal ikke lese av noen globale verdier, den skal kun ta i mot products som parameter, og ta inn en potensiel discount verdi. 
+/* Som sist har du kun lov å legge til din egen funksjon, den skal ikke lese av noen globale verdier,
+ den skal kun ta i mot products som parameter, og ta inn en potensiel discount verdi. 
     Her må du også skjekke at discount ikke er mindre enn 0, og ikke større enn 100. 
 */
 
@@ -35,6 +30,9 @@ console.log(products);
 
 //!!Skriv inn funksjonen din totalSum her:
 
+function totalSum(){
+    
+}
 
 /* Her ser du at vi forventer en funksjon som tar inn produktet, og et potensielt avlag, or returnerer totalkostnadden av alle produktene.  */
 console.log(totalSum(products));
